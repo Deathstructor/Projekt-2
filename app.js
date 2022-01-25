90cda252e12647138aa40172996cb53ffff43a6d
Paul Chastain <paulchast1@gmail.com>
2022-01-25 14:35:47 +0100

    stuff

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 let cookieAmount = 0
-let cookieIncrease = 1
+let cookieClick = 1
 let cpsAmount = 0
 let cookieButton = document.querySelector(".cookieButton")
 let cookieCounter = document.querySelector(".cookieCounter")
 let buyAutoclicker = document.querySelector(".buy-autoclicker")
 let cps = document.querySelector(".cps")
-let autoclickerPrice = document.querySelector();
+let autoclickerPrice = document.querySelector(".autoclicker h3");
+
+let acPrice = 20;
 
 cookieButton.addEventListener("click", cookie)
 buyAutoclicker.addEventListener("click", autoclicker)
 
 function cookie() {
-    cookieAmount += cookieIncrease
+    cookieAmount += cookieClick
     cookieCounter.innerHTML = "Cookie amount: " + cookieAmount
 }
 
 function autoclicker() {
-    let price = 20
-    if (cookieAmount >= price) {
-        cookieAmount -= price
-        price *= 1.1
+    if (cookieAmount >= Math.round(acPrice)) {
+        cookieAmount -= Math.round(acPrice)
+        acPrice *= 1.1
         cpsAmount += 1
         cps.innerHTML = "CPS: " + cpsAmount
         cookieCounter.innerHTML = "Cookie amount: " + cookieAmount
-        autoclicker.innerHTML
+        autoclickerPrice.innerHTML = "Price: " + Math.round(acPrice)
     }
+    console.log("Autoclicker " + acPrice)
 }
