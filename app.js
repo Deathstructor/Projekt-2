4ba41521ce57dda9f511b2327642a2d5e3916ab8
Paul Chastain <paulchast1@gmail.com>
2022-02-01 13:32:53 +0100

    små förbättringar

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-let cookieAmount = 0
-let cookieClick = 1
-let cpsAmount = 0
+let cookieAmount = 0, cookieClick = 1
+let cpsAmount = 0, cps = document.querySelector(".cps")
+
 let cookieButton = document.querySelector(".cookieButton")
 let cookieCounter = document.querySelector(".cookieCounter")
-let buyAutoclicker = document.querySelector(".buy-autoclicker")
-let cps = document.querySelector(".cps")
-let autoclickerPrice = document.querySelector(".autoclicker h3");
+let buyAutoclicker = document.querySelector(".buy-autoclicker"), autoclickerAmount = document.querySelector(".acAmount") , autoclickerPrice = document.querySelector(".acPrice")
 
-let acPrice = 20;
+let acAmount = 0, acPrice = 20;
 
 cookieButton.addEventListener("click", cookie)
 buyAutoclicker.addEventListener("click", autoclicker)
@@ -17,14 +15,20 @@ function cookie() {
     cookieCounter.innerHTML = "Cookie amount: " + cookieAmount
 }
 
+setInterval(function () {
+    cookieAmount += cpsAmount
+    cookieCounter.innerHTML = "Cookie amount: " + cookieAmount
+}, 1000)
+
 function autoclicker() {
     if (cookieAmount >= Math.round(acPrice)) {
         cookieAmount -= Math.round(acPrice)
-        acPrice *= 1.1
+        acPrice *= 1.3
         cpsAmount += 1
+        acAmount += 1
         cps.innerHTML = "CPS: " + cpsAmount
         cookieCounter.innerHTML = "Cookie amount: " + cookieAmount
+        autoclickerAmount.innerHTML = "Amount: " + acAmount
         autoclickerPrice.innerHTML = "Price: " + Math.round(acPrice)
     }
-    console.log("Autoclicker " + acPrice)
 }
