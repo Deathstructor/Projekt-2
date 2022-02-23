b7b41d98a45356e7cb7201d9d5d2b0018d5f4095
Paul Chastain <paulchast1@gmail.com>
2022-02-23 12:13:47 +0100

    små förändringar / förbättringar

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ let cpsAmount = 0, cps = document.querySelector(".cps")
 
 let cookieButton = document.querySelector(".cookieButton"), cookieImg = document.querySelector(".cookieImg")
 let cookieCounter = document.querySelector(".cookieCounter")
+let buyClicks = document.querySelector(".buy-clicks"), clicksAmount = document.querySelector(".clAmount"), clicksPrice = document.querySelector(".clPrice")
 let buyAutoclicker = document.querySelector(".buy-autoclicker"), autoclickerAmount = document.querySelector(".acAmount") , autoclickerPrice = document.querySelector(".acPrice")
 let buyGrandma = document.querySelector(".buy-grandma"), grandmaAmount = document.querySelector(".grAmount"), grandmaPrice = document.querySelector(".grPrice")
 
-let acAmount = 0, acPrice = 20
-let grAmount= 0, grPrice = 200
+let clAmount = 0, clPrice = 100
+let acAmount = 0, acPrice = 1000
+let grAmount= 0, grPrice = 2500
 
 cookieButton.addEventListener("click", cookie)
+buyClicks.addEventListener("click", clicks)
 buyAutoclicker.addEventListener("click", autoclicker)
 buyGrandma.addEventListener("click", grandma)
 
@@ -23,10 +26,23 @@ setInterval(function () {
     cookieCounter.innerHTML = "Cookie amount: " + cookieAmount
 }, 1000)
 
+function clicks() {
+    if (cookieAmount >= Math.round(clPrice)) {
+        cookieAmount -= Math.round(clPrice)
+        clPrice *= 1.35
+        cookieClick += 1
+        clAmount ++
+        cps.innerHTML = "CPS: " + cpsAmount
+        cookieCounter.innerHTML = "Cookie amount: " + cookieAmount
+        clicksAmount.innerHTML = "Amount: " + clAmount
+        clicksPrice.innerHTML = "Price: " + Math.round(clPrice)
+    }
+}
+
 function autoclicker() {
     if (cookieAmount >= Math.round(acPrice)) {
         cookieAmount -= Math.round(acPrice)
-        acPrice *= 1.2
+        acPrice *= 1.35
         cpsAmount += 1
         acAmount ++
         cps.innerHTML = "CPS: " + cpsAmount
