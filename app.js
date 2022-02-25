648d3cfb1925860ca9c241bfe995bb62432f945c
Paul Chastain <paulchast1@gmail.com>
2022-02-25 13:30:35 +0100

    added timer and goal

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ let cpsAmount = 0, cps = document.querySelector(".cps")
 
 let cookieButton = document.querySelector(".cookieButton"), cookieImg = document.querySelector(".cookieImg")
 let cookieCounter = document.querySelector(".cookieCounter")
+let timer = document.querySelector(".timer"), goal = document.querySelector(".goal")
+let minutes = 4, seconds = 60, cookieGoal = 5000
+
 let buyClicks = document.querySelector(".buy-clicks"), clicksAmount = document.querySelector(".clAmount"), clicksPrice = document.querySelector(".clPrice")
 let buyAutoclicker = document.querySelector(".buy-autoclicker"), autoclickerAmount = document.querySelector(".acAmount") , autoclickerPrice = document.querySelector(".acPrice")
 let buyGrandma = document.querySelector(".buy-grandma"), grandmaAmount = document.querySelector(".grAmount"), grandmaPrice = document.querySelector(".grPrice")
 
-let clAmount = 0, clPrice = 100
-let acAmount = 0, acPrice = 1000
-let grAmount= 0, grPrice = 2500
+let clAmount = 0, clPrice = 150
+let acAmount = 0, acPrice = 500
+let grAmount = 0, grPrice = 1500
+
 
 cookieButton.addEventListener("click", cookie)
 buyClicks.addEventListener("click", clicks)
@@ -21,9 +25,32 @@ function cookie() {
     cookieCounter.innerHTML = "Cookie amount: " + cookieAmount
 }
 
-setInterval(function () {
+setInterval(function() {
     cookieAmount += cpsAmount
     cookieCounter.innerHTML = "Cookie amount: " + cookieAmount
+
+
+
+    seconds -= 1
+    if (seconds > 9) {
+        timer.innerHTML = "Time: " + minutes + ":" + seconds
+    }
+    else if (seconds < 10) {
+        timer.innerHTML = "Time: " + minutes + ":0" + seconds
+    }
+
+    if (seconds == 0) {
+        minutes -= 1
+        seconds += 60
+    }
+
+    if (minutes == 0) {
+        if (cookieAmount >= cookieGoal) {
+            cookieAmount -= cookieGoal
+            cookieGoal *= 3
+            goal.innerHTML = "Goal: " + cookieGoal
+        }
+    }
 }, 1000)
 
 function clicks() {
